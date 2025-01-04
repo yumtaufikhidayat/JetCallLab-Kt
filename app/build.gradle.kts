193ad4cb710bf2320f26d180ba33966ff163a4b4
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-01-04 20:10:39 +0700

    chore: Migrate lifecycle-viewmodel-compose dependency to version catalog
    - Replace hardcoded dependency string "androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5" with `libs.androidx.lifecycle.viewmodel.compose`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     // Permission
     implementation("com.google.accompanist:accompanist-permissions:0.37.3")
